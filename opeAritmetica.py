7651af9e0c09c04be0fafd43987e055aed8393e5
Jorge Avila <jorgelars18@gmail.com>
2024-05-11 16:55:37 -0400

    primer repo

---
+++ opeAritmetica.py
@@ -0,0 +1,19 @@
+from variables import numero1
+
+print("--------------------------------------------")
+print(numero1)
+num1 = 1
+
+num2 =3
+
+suma = 2+2
+print(num1 + num2)
+
+print(suma)
+
+numero2= input("Ingresa el primer numero: ")
+numero3= input("Ingresa el segundo numero: ")
+suma = numero2  + numero3
+suma = int(numero3) + int(numero2)
+
+print(suma)
---
+++ variables.py
@@ -0,0 +1,14 @@
+numero1 = 13
+
+entero = 10
+decimal = 10.5
+
+texto = "hola mundo"
+
+print(entero)
+print(decimal)
+print(texto)
+
+print(type(entero))
+print(type(decimal))
+print(type(texto))
